ab153881ffe0313cc8a11036ac55017d9c3ff4d2
David Shugert <shugertdavid@gmail.com>
2020-02-11 13:19:16 -0600

    Updates

---
+++ precache-manifest.98d8fa336860e34908f25d0006a21a74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d5334a2354c4ad8fa51775166c3f3f",
+    "url": "/reactPWA/index.html"
+  },
+  {
+    "revision": "2581e1754a5ae464c222",
+    "url": "/reactPWA/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b55d60e960b1d0eabd84",
+    "url": "/reactPWA/static/css/main.29097b8e.chunk.css"
+  },
+  {
+    "revision": "2581e1754a5ae464c222",
+    "url": "/reactPWA/static/js/2.07b4f1c1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactPWA/static/js/2.07b4f1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b55d60e960b1d0eabd84",
+    "url": "/reactPWA/static/js/main.fa4da710.chunk.js"
+  },
+  {
+    "revision": "9e3379cb216a276b318b",
+    "url": "/reactPWA/static/js/runtime-main.184ecb6d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reactPWA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
